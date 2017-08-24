967edd1e120601e92d0ea608eb5993442f7efabb
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2017-08-24 18:22:09 -0500

    added numRef, changed dimeDetalles, cambiaNumRef works

--- Libro.java
+++ Libro.java
@@ -4,12 +4,14 @@ public class Libro
     private String titulo;
     private String autor;
     private int paginas;
+    private String numReferencia;
     
     public Libro(String bkTitulo, String bkAutor, int bkPages)
     {
         titulo = bkTitulo;
         autor = bkAutor;
         paginas = bkPages;
+        numReferencia = "";
     }
 
     String dimeAutor()
@@ -29,12 +31,33 @@ public class Libro
     
     public String dimeDetalles()
     {
-        String cadDetails = "Titulo: ";
+        String cadDetails = " ";
+        cadDetails += "Titulo: ";
         cadDetails += dimeTitulo();
         cadDetails += ", Autor: ";
         cadDetails += dimeAutor();
         cadDetails += ", Paginas: ";
         cadDetails += dimePaginas();
+        int counter = numReferencia.length();
+        if(counter > 0)
+        {
+            cadDetails += ", num de ref: ";
+            cadDetails += dimeNumRef();
+        }
+        else
+            cadDetails += ", num de ref: ";
+            cadDetails += "ZZZ";
         return cadDetails;
+    }
+    
+    String cambiaNumRef(String nRef)
+    {
+        numReferencia = nRef;
+        return numReferencia;
+    }
+    
+    String dimeNumRef()
+    {
+        return numReferencia;
     }
 }
