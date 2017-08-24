bdc32acf5d4f5a28c2eb8503984664eebbce3ba5
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2017-08-24 10:39:29 -0500

    creado libro con constructor

---
+++ Libro.java
@@ -0,0 +1,14 @@
+
+public class Libro
+{
+    private String titulo;
+    private String autor;
+    
+    public Libro(String bkTitulo, String bkAutor)
+    {
+        titulo = bkTitulo;
+        autor = bkAutor;        
+    }
+
+    
+}
