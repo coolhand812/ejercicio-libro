47e89e6083f646addeaf96e86c90b643222986b7
Joe Diaz <coolhand812@alumnos.uaslp.edu.mx>
2017-08-31 20:10:05 -0500

    Ensured all steps and documentation were completed.

--- Libro.java
+++ Libro.java
@@ -1,4 +1,8 @@
-
+/**
+ * Esta clase controla libros.
+ * @Joe Diaz
+ * @ version 1.0
+ */
 public class Libro
 {
     private String titulo;
@@ -6,7 +10,12 @@ public class Libro
     private int paginas;
     private String numReferencia;
     private int prestamos;
-    
+    /**
+     * Este es el constructor de la clase Libro.
+     * @bkTitulo Referencia para titulo
+     * @bkAutor Referencia para autor
+     * @bkPages Referencia para paginas
+     */
     public Libro(String bkTitulo, String bkAutor, int bkPages)
     {
         titulo = bkTitulo;
@@ -16,21 +25,33 @@ public class Libro
         prestamos = 0;
     }
 
+    /**
+     * Este metodo regresa el nombre del autor.
+     */
     String dimeAutor()
     {
         return autor;
     }
     
+    /**
+     * Este metodo regresa el nombre del titulo.
+     */
     String dimeTitulo()
     {
         return titulo;
     }
     
+    /**
+     * Este metodo regresa el numero de paginas del libro.
+     */
     int dimePaginas()
     {
         return paginas;
     }
     
+    /**
+     * Este metodo enseña los detalles del libro en la pantalla.
+     */
     public String dimeDetalles()
     {
         String cadDetails = " ";
@@ -54,6 +75,10 @@ public class Libro
         return cadDetails;
     }
     
+    /**
+     * Este metodo cambia el numero de referencia.
+     * @nRef Es una variable que recibe el numero de referencia.
+     */
     String cambiaNumRef(String nRef)
     {
         int counter = nRef.length();
@@ -64,16 +89,25 @@ public class Libro
         return numReferencia;
     }
     
+    /**
+     * Este metodo regresa el numero de referencia.
+     */
     String dimeNumRef()
     {
         return numReferencia;
     }
     
+    /**
+     * Este metodo maneja cuantas veces se ha prestado un libro.
+     */
     void libPrestar()
     {
         prestamos+=1;
     }
     
+    /**
+     * Este metodo regresa el numero de veces que se ha prestado un libro.
+     */
     int dimePrestamos()
     {
         return prestamos;
